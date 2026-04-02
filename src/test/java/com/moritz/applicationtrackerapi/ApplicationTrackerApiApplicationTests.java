a8f644e639fb4464954e463b20763de8248cfbba
moritz <moritzhild@web.de>
2026-04-02 18:17:12 +0200

    Initial Spring Boot project setup 2

---
+++ src/test/java/com/moritz/applicationtrackerapi/ApplicationTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moritz.applicationtrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
